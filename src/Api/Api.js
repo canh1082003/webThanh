61fdc5ea49115ce088849ab24d4b9001a8241214
canh1082003 <anhnbk113@gmail.com>
2024-05-04 21:38:24 +0700

    fix

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -6,7 +6,7 @@ class Http {
   constructor() {
     this.instance = axios.create({
       baseURL: "http://localhost:4000/api/",
-      timeout: 1000,
+      timeout: 10000,
       headers: { "X-Custom-Header": "foobar" },
     });
 
--- src/Page/admin/ListProduct.jsx
+++ src/Page/admin/ListProduct.jsx
@@ -285,7 +285,7 @@ export default function ListProduct() {
               </button>
               <button
                 className="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg px-5 py-2.5 text-center"
-                onClick={() => handleDelete(productId)}
+                onClick={() => handleDelete(product.id)}
               >
                 Delete
               </button>
